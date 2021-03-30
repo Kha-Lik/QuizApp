1543f673a9746b165fa44e1a84d90a213f7b878f
MykolaV <kolya.vasevich@gmail.com>
2021-03-30 21:23:07 +0300

    added DbContext

---
+++ Quiz_DAL/Implementations/QuizDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Quiz_DAL.Entities;
+
+namespace Quiz_DAL.Implementations
+{
+    public class QuizDbContext : IdentityDbContext<User>
+    {
+        public QuizDbContext(DbContextOptions<QuizDbContext> options) : base(options)
+        {
+            base.Database.EnsureCreated();
+        }
+        
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Attempt> Attempts { get; set; }
+        public DbSet<QuestionResult> QuestionResults { get; set; }
+    }
+}
