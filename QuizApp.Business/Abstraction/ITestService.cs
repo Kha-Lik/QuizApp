a0f5722ac5962c9e9bac529f94f4c322109e7238
MykolaV <kolya.vasevich@gmail.com>
2021-04-14 21:24:06 +0300

    created ITestService.cs

---
+++ QuizApp.Business/Abstraction/ITestService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using QuizApp.Business.Models;
+
+namespace QuizApp.Business.Abstraction
+{
+    public interface ITestService
+    {
+        Task<IEnumerable<AttemptDto>> GetTestResultsForStudentAsync(UserDto student, int topicId);
+        Task<TestModel> GenerateTestForTopicAsync(UserDto student, int topicId);
+        Task SubmitTestAsync(TestModel generatedTest);
+    }
+}
