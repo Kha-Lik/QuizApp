5d2ee9e3fdf8525c20779638a0fd3b3703cac902
MykolaV <kolya.vasevich@gmail.com>
2021-04-14 21:23:47 +0300

    create model for generated tests

---
+++ QuizApp.Business/Models/TestModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuizApp.Business.Models
+{
+    public class TestModel
+    {
+        public UserDto Student { get; set; }
+        public TopicDto Topic { get; set; }
+        public DateTime DateTimePassed { get; set; }
+        public int Score { get; set; }
+        public IEnumerable<QuestionDto> Questions { get; set; }
+    }
+}
