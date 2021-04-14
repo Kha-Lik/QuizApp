48e96472f2a23131f16dd1daba191021d5e23386
MykolaV <kolya.vasevich@gmail.com>
2021-04-14 21:16:51 +0300

    changed Id field type to string, removed unnecessary fields

--- QuizApp.DataAccess/Entities/Topic.cs
+++ QuizApp.DataAccess/Entities/Topic.cs
@@ -6,8 +6,11 @@ namespace QuizApp.DataAccess.Entities
     {
         public string Name { get; set; }
         public int TopicNumber { get; set; }
-        public IEnumerable<Question> Questions { get; set; }
-        public int SubjectId { get; set; }
+        public int TimeToPass { get; set; }
+        public int QuestionsPerAttempt { get; set; }
+        public int MaxAttemptCount { get; set; }
+        
+        public string SubjectId { get; set; }
         public Subject Subject { get; set; }
     }
 }
