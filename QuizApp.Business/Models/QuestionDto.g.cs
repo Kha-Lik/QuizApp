3850ff934797830b1a278a9a63c0d28ae03c3a73
MykolaV <kolya.vasevich@gmail.com>
2021-04-16 16:22:13 +0300

    added Answers collection to QuestionDto

--- QuizApp.Business/Models/QuestionDto.g.cs
+++ QuizApp.Business/Models/QuestionDto.g.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace QuizApp.Business.Models
 {
     public partial class QuestionDto
@@ -5,5 +7,6 @@ namespace QuizApp.Business.Models
         public int QuestionNumber { get; set; }
         public string TopicId { get; set; }
         public string Id { get; set; }
+        public ICollection<AnswerDto> Answers { get; set; }
     }
 }
