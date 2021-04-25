64268c63c65ffff7138fea6e8ff173beb4faf9a7
Pawtetka <45591360+Pawtetka@users.noreply.github.com>
2021-04-26 02:58:47 +0300

    Fix validation bug (add UserValidator)

--- QuizApp.Business/Extensions/ServiceCollectionExtension.cs
+++ QuizApp.Business/Extensions/ServiceCollectionExtension.cs
@@ -28,7 +28,8 @@ namespace QuizApp.Business.Extensions
                 .AddTransient<AbstractValidator<AnswerDto>, AnswerValidator>()
                 .AddTransient<AbstractValidator<AttemptDto>, AttemptValidator>()
                 .AddTransient<AbstractValidator<QuestionResultDto>, QuestionResultValidator>()
-                .AddTransient<AbstractValidator<TestModel>, TestValidator>();
+                .AddTransient<AbstractValidator<TestModel>, TestValidator>()
+                .AddTransient<AbstractValidator<UserDto>, UserValidator>();
 
             return services;
         }
---
+++ QuizApp.Business/Implementation/Validation/UserValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using QuizApp.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp.Business.Implementation.Validation
+{
+    public class UserValidator : AbstractValidator<UserDto>
+    {
+        public UserValidator()
+        {
+            RuleFor(a => a.Id).NotNull();
+            RuleFor(a => a.Name).NotEmpty();
+            RuleFor(a => a.Surname).NotEmpty();
+        }
+    }
+}
