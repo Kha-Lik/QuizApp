0c5753f2f2e39461b6c797e3aaacb5513e3886e7
MykolaV <kolya.vasevich@gmail.com>
2021-04-30 19:20:31 +0300

    added project typedefs

---
+++ QuizApp.API/client-app/src/appTypes.d.ts
@@ -0,0 +1,30 @@
+export interface Subject {
+    Id : string,
+    LecturerId : string,
+    Name : string
+}
+
+export interface Topic{
+    Id : string,
+    SubjectId : string,
+    Name : string,
+    TopicNumber : number,
+    TimeToPass : number,
+    QuestionsPerAttempt : number,
+    MaxAttemptCount : number
+}
+
+export interface Question{
+    Id: string,
+    TopicId: string,
+    QuestionNumber: number,
+    QuestionText: string,
+    Answers: Answer[]
+}
+
+export interface Answer{
+    Id: string,
+    QuestionId: string,
+    Answertext: string,
+    IsCorrect: boolean
+}
