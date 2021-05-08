a1aa08848c0db60b6f2d5564fb4fd2cc9e6a10a1
MykolaV <kolya.vasevich@gmail.com>
2021-05-08 17:05:45 +0300

    added modal form for subject creation

---
+++ QuizApp.API/client-app/src/components/modalSubjectCreate.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {makeStyles, Theme, createStyles} from "@material-ui/core/styles";
+import {Backdrop, Fade, Modal, Paper} from "@material-ui/core";
+import SubjectCreationForm from "./subjectCreationForm";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        modal: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+        },
+    })
+);
+
+export interface ModalSubjectCreateProps {
+    openState: boolean;
+    doClose: Function;
+}
+
+export default function ModalSubjectCreate({
+                                               openState,
+                                               doClose,
+                                           }: ModalSubjectCreateProps) {
+    const classes = useStyles();
+
+    return (
+        <Modal
+            id="subjecCreation"
+            className={classes.modal}
+            open={openState}
+            onClose={() => doClose()}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+                timeout: 500,
+            }}
+        >
+            <Fade in={openState}>
+                <Paper elevation={5} variant="outlined">
+                    <SubjectCreationForm onSubmit={() => doClose()}/>
+                </Paper>
+            </Fade>
+        </Modal>
+    );
+}
