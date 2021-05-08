08d7f34b03fba84bc2a28437215d6aee839c3ee3
MykolaV <kolya.vasevich@gmail.com>
2021-05-09 02:06:28 +0300

    added more custom type declarations

--- QuizApp.API/client-app/src/appTypes.d.ts
+++ QuizApp.API/client-app/src/appTypes.d.ts
@@ -28,3 +28,34 @@ export interface Answer {
     AnswerText: string,
     IsCorrect: boolean
 }
+
+export interface Attempt {
+    Id: string;
+    StudentId: string;
+    TopicId: string;
+    Score: number;
+    DateTime: Date;
+}
+
+export interface AttemptViewModel {
+    Id: string;
+    StudentName: string;
+    TopicName: string;
+    SubjectName: string;
+    Score: number;
+    DateTime: string;
+}
+
+export interface JwtUser {
+    Sub: string;
+    Jti: string;
+    NameIdentifier: string;
+    Role: string;
+}
+
+export interface User {
+    Id: string;
+    Name: string;
+    Surname: string;
+    Role: string;
+}
