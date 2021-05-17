78a5ac481394a7c16b018df5cde3f5c3c5b8d996
Pawtetka <45591360+Pawtetka@users.noreply.github.com>
2021-05-17 17:57:12 +0300

    Added AnswerController and AttemptController

---
+++ QuizApp.API/Controllers/AttemptController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using QuizApp.Business.Abstraction;
+using QuizApp.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttemptController : ControllerBase
+    {
+        private readonly ICrudInterface<AttemptDto> _attemptService;
+        public AttemptController(ICrudInterface<AttemptDto> attemptService)
+        {
+            _attemptService = attemptService;
+        }
+
+        [Authorize(Roles = "Lecturer, Student")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AttemptDto>>> GetAll()
+        {
+            return await _attemptService.GetAllAsync().ToListAsync();
+        }
+
+        [Authorize(Roles = "Lecturer, Student")]
+        [HttpGet("Get by id = {id}")]
+        public async Task<ActionResult<AttemptDto>> GetById(string id)
+        {
+            return await _attemptService.GetByIdAsync(id);
+        }
+
+        [Authorize(Roles = "Lecturer, Student")]
+        [HttpGet("Get by subject id = {id}")]
+        public async Task<ActionResult<IEnumerable<AttemptDto>>> GetBySubjectId(string id)
+        {
+            return await _attemptService.GetEntitiesByPrincipalId(id).ToListAsync();
+        }
+
+        [Authorize(Roles = "Lecturer")]
+        [HttpPost]
+        public async Task<ActionResult<AttemptDto>> Create(AttemptDto attemptDto)
+        {
+            try
+            {
+                await _attemptService.CreateEntityAsync(attemptDto);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return Ok(attemptDto);
+        }
+
+        [Authorize(Roles = "Lecturer")]
+        [HttpPut]
+        public async Task<ActionResult<AttemptDto>> Update(AttemptDto attemptDto)
+        {
+            await _attemptService.UpdateEntity(attemptDto);
+
+            return Ok(attemptDto);
+        }
+
+        [Authorize(Roles = "Lecturer")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            await _attemptService.DeleteEntityByIdAsync(id);
+
+            return Ok();
+        }
+    }
+}
