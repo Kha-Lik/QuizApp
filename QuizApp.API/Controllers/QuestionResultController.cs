993b68f4d5ed89d17a15d4a292b19fe00f1fd80d
Pawtetka <45591360+Pawtetka@users.noreply.github.com>
2021-05-17 17:59:14 +0300

    Add QuestionResultController

---
+++ QuizApp.API/Controllers/QuestionResultController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuizApp.Business.Abstraction;
+using QuizApp.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionResultController : ControllerBase
+    {
+        private readonly ICrudInterface<QuestionResultDto> _questionResultService;
+        public QuestionResultController(ICrudInterface<QuestionResultDto> questionResultService)
+        {
+            _questionResultService = questionResultService;
+        }
+
+        [Authorize(Roles = "Lecturer, Student")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<QuestionResultDto>>> GetAll()
+        {
+            return await _questionResultService.GetAllAsync().ToListAsync();
+        }
+
+        [Authorize(Roles = "Lecturer, Student")]
+        [HttpGet("Get by id = {id}")]
+        public async Task<ActionResult<QuestionResultDto>> GetById(string id)
+        {
+            return await _questionResultService.GetByIdAsync(id);
+        }
+
+        [Authorize(Roles = "Lecturer, Student")]
+        [HttpGet("Get by subject id = {id}")]
+        public async Task<ActionResult<IEnumerable<QuestionResultDto>>> GetBySubjectId(string id)
+        {
+            return await _questionResultService.GetEntitiesByPrincipalId(id).ToListAsync();
+        }
+
+        [Authorize(Roles = "Lecturer")]
+        [HttpPost]
+        public async Task<ActionResult<QuestionResultDto>> Create(QuestionResultDto questionResultDto)
+        {
+            try
+            {
+                await _questionResultService.CreateEntityAsync(questionResultDto);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return Ok(questionResultDto);
+        }
+
+        [Authorize(Roles = "Lecturer")]
+        [HttpPut]
+        public async Task<ActionResult<QuestionResultDto>> Update(QuestionResultDto questionResultDto)
+        {
+            await _questionResultService.UpdateEntity(questionResultDto);
+
+            return Ok(questionResultDto);
+        }
+
+        [Authorize(Roles = "Lecturer")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(string id)
+        {
+            await _questionResultService.DeleteEntityByIdAsync(id);
+
+            return Ok();
+        }
+    }
+}
